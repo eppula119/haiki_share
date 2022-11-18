783ab8ac2017b5f41fe9a37596f6c28de955a8a8
noguchi <noguchi@electrodream.co.jp>
2022-11-18 15:26:55 +0900

    システムエラーページの見た目修正

--- resources/js/components/Error.vue
+++ resources/js/components/Error.vue
@@ -1,3 +1,11 @@
 <template>
-  <p>システムエラーが発生しました。</p>
+  <div class="p-error">
+    <p class="p-error__text">
+      システムエラーが発生しました。<br>
+      ページを再読み込みするか、
+      <RouterLink class="p-errorLink" to="/">トップページ</RouterLink>へ移動し<br>
+      再度操作を行ってください。
+    </p>
+    
+  </div>
 </template>
